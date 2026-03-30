d31ab11c6b0b846b503e454c1e58ff5285bf7e55
a-tann <a.tan@digipen.edu>
2026-03-30 22:41:38 +0800

    update credit scene

--- ChronoGame/Scripts/SplashScreen_ReturnToMainMenu.hpp
+++ ChronoGame/Scripts/SplashScreen_ReturnToMainMenu.hpp
@@ -16,7 +16,7 @@ public:
     void Awake() override {}
     void Initialize(Entity) override {}
     void Start() override {
-        Events::Listen("SplashScreenDone", [this](void*) {
+        Events::Listen("SplashScreenDone", [](void*) {
             NE::Scripting::SwitchScene("7f6eb653-c6f7-426d-b9ac-9c0bada73cfc");
         });
     }
